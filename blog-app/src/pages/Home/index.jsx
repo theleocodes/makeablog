333d9a570377c73a635865afca4b466819868282
Aditya Sharma <adityashf7@gmail.com>
2022-11-17 13:39:44 +0530

    Main

---
+++ blog-app/src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "../../components/Home/Header"
+import SearchBar from "../../components/Home/SearchBar"
+// import BlogList from "../../components/Home/BlogList"
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <SearchBar />
+        {/* <BlogList /> */}
+    </div>
+  )
+}
+
+export default Home
