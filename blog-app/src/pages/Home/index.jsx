e50649425a70f6537ac037c71faf11eb9356ce61
Aditya Sharma <adityashf7@gmail.com>
2022-11-22 00:03:23 +0530

    Final commit

--- blog-app/src/pages/Home/index.jsx
+++ blog-app/src/pages/Home/index.jsx
@@ -1,16 +1,53 @@
-import React from "react"
+
 import Header from "../../components/Home/Header"
 import SearchBar from "../../components/Home/SearchBar"
-// import BlogList from "../../components/Home/BlogList"
+import BlogList from "../../components/Home/BlogList"
+import EmptyList from "../../components/common/EmptyList"
+import { blogList } from "../../config/data"
+import React, { useState } from "react"
 
 const Home = () => {
+  const [blogs, setBlogs] = useState(blogList);
+  const [searchKey, setSearchKey] = useState('');
+
+  // Search submit
+  const handleSearchBar = (e) => {
+    e.preventDefault();
+    handleSearchResults();
+  };
+
+  // Search for blog by category
+  const handleSearchResults = () => {
+    const allBlogs = blogList;
+    const filteredBlogs = allBlogs.filter((blog) =>
+      blog.category.toLowerCase().includes(searchKey.toLowerCase().trim())
+    );
+    setBlogs(filteredBlogs);
+  };
+
+  // Clear search and show all blogs
+  const handleClearSearch = () => {
+    setBlogs(blogList);
+    setSearchKey('');
+  };
+
   return (
     <div>
-        <Header />
-        <SearchBar />
-        {/* <BlogList /> */}
-    </div>
-  )
-}
+      {/* Page Header */}
+      <Header />
 
-export default Home
+      {/* Search Bar */}
+      <SearchBar
+        value={searchKey}
+        clearSearch={handleClearSearch}
+        formSubmit={handleSearchBar}
+        handleSearchKey={(e) => setSearchKey(e.target.value)}
+      />
+
+      {/* Blog List & Empty View */}
+      {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
+    </div>
+  );
+};
+
+export default Home;
